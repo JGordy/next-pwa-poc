70e9f216c017ac02809d8d5e84a076f932ee1412
Joseph Gordy <jgordy2424@gmail.com>
2023-06-22 16:27:53 -0400

    update next config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,16 @@
-/** @type {import('next').NextConfig} */
-const withPWA = require('next-pwa');
-
-module.exports = withPWA({
-    // next.js config
-    dest: "public",
+const withPWA = require('next-pwa')({
+    dest: 'public',
     register: true,
     skipWaiting: true,
+    // disable: process.env.NODE_ENV === 'development',
+    buildExcludes: [/middleware-manifest.json$/]
 });
+
+const nextConfig = {
+    reactStrictMode: true,
+    experimental: {
+        appDir: true,
+    }
+};
+
+module.exports = withPWA(nextConfig);
